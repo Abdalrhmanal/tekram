3062ac7843d9d54c977a2026d26fb50f0fb37cad
abdulrahman_sr <homjpkatarhom6468@gmail.com>
2025-05-24 00:03:55 +0300

    Refactor Customers component: update columns for customer data display and enhance rendering logic with tooltips and avatars

--- src/app/(project)/users/customers/page.tsx
+++ src/app/(project)/users/customers/page.tsx
@@ -4,12 +4,14 @@ import React from 'react'
 function Customers() {
     const dataSourceName = "api/customers";
     const columns = [
-        { headerName: "ID", field: "id", sortable: true },
-        { headerName: "Name", field: "company_name", sortable: false },
-        { headerName: "description", field: "description", sortable: true },
-        { headerName: "company_email", field: "company_email", sortable: true },
-        { headerName: "company_address", field: "company_address", sortable: true },
+        { headerName: "Name", field: "name", sortable: true },
+        { headerName: "City", field: "city", sortable: true },
+        { headerName: "Phone Number", field: "phone_number", sortable: true },
+        { headerName: "Address", field: "address", sortable: true },
+        { headerName: "Status", field: "is_active", sortable: true },
+        { headerName: "wallet", field: "walletId", sortable: true },
     ];
+   
     return (
         <>
             <GridTable dataSourceName={dataSourceName} columns={columns} />
--- src/components/data-table2/renderCell/renderCell.tsx
+++ src/components/data-table2/renderCell/renderCell.tsx
@@ -1,11 +1,39 @@
 import React from "react";
-import { Typography, Chip, Grid, Avatar } from "@mui/material";
+import { Typography, Chip, Grid, Avatar, Tooltip } from "@mui/material";
 
 export const renderCell = (field: string, value: any, row: any): React.ReactNode => {
+    const truncateText = (text: string, maxLength: number = 15) =>
+        text.length > maxLength ? text.substring(0, maxLength) + "..." : text;
+
     switch (field) {
-        case "company_name":
+        case "name":
             return (
-                <Typography fontWeight="bold" sx={{ color: "red" }}>{row.company_name || ''}</Typography>
+                <>
+                    <Grid container spacing={2} alignItems="center">
+                        <Grid size={2}>
+                            <Avatar alt={row.name} src={`http://145.223.116.44:9993/${row.avatar}`} />
+                        </Grid>
+
+                        <Grid size={10}>
+                            <Grid container spacing={0.5}>
+                                <Grid size={12}>
+                                    <Tooltip title={row.name} arrow>
+                                        <Typography fontWeight="bold">
+                                            {truncateText(row.name)}
+                                        </Typography>
+                                    </Tooltip>
+                                </Grid>
+                                <Grid size={12}>
+                                    <Tooltip title={row.email} arrow>
+                                        <Typography fontWeight="bold">
+                                            {truncateText(row.email)}
+                                        </Typography>
+                                    </Tooltip>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </>
             );
 
         case "date":
@@ -24,11 +52,19 @@ export const renderCell = (field: string, value: any, row: any): React.ReactNode
             const formattedcreated_at = row.created_at ? new Intl.DateTimeFormat('en-GB', {
                 year: 'numeric',
                 month: '2-digit',
-                day: '2-digit',             
+                day: '2-digit',
             }).format(new Date(row.created_at)) : '';
 
             return <>{formattedcreated_at}</>;
-
+        case "is_active":
+            return (
+                <Chip
+                    label={value ? "Active" : "Inactive"}
+                    color={value ? "success" : "error"}
+                    variant="outlined"
+                    size="small"
+                />
+            );
         default:
             return value ?? "-";
     }
