e06acb693ebe9fb9a6dcb588dd2d6902fc77fd44
abdulrahman_sr <homjpkatarhom6468@gmail.com>
2025-05-26 13:35:58 +0300

    Add initial data handling to CreateCustomar component for edit mode in DynamicForm

--- src/app/(project)/users/service-providers/create/page.tsx
+++ src/app/(project)/users/service-providers/create/page.tsx
@@ -7,9 +7,22 @@ function CreateCustomar() {
     const handleSubmit = (data: FormData) => {
         console.log('Submitted Data:', data);
     };
+    const initialData: FormData = {
+    city: 'حلب',
+    name: 'علي حمضي',
+    address: 'جمعية الزهراء',
+    email: 'kareem.happal@gmail.com',
+    phone: '0978654111',
+    services: ['أجار شاليهات', 'أجار غرف فندقية'],
+    // يمكنك إضافة صورة مبدئية إذا كنت ترغب في ذلك
+    // image: new File([''], 'example.jpg', { type: 'image/jpeg' }),
+  };
+
     return (
         <>
             <DynamicForm mode="add" onSubmit={handleSubmit} />
+
+            <DynamicForm mode="edit" initialData={initialData} onSubmit={handleSubmit} />
         </>
     )
 }
