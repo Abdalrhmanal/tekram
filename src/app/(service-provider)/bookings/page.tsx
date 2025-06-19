8f63e4d4ebbbc6168d389497e4eb3169b8af77a9
abdulrahman_sr <homjpkatarhom6468@gmail.com>
2025-06-19 18:10:54 +0300

    feat: Refactor BookingsProvider component to clean up commented code and enhance tab navigation with Tabber component

--- src/app/(service-provider)/bookings/page.tsx
+++ src/app/(service-provider)/bookings/page.tsx
@@ -1,27 +1,30 @@
+import Calendar from '@/components/calendar';
 import GridTable from '@/components/data-table2';
 import { ComparisonOperator } from '@/components/data-table2/type/type';
 import GridCard from '@/components/grid-card';
+import Tabber from '@/components/tabber';
+import { Deblur } from '@mui/icons-material';
 import React, { Suspense } from 'react'
 
 function BookingsProvider() {
 
-    const dataSourceName = "api/bookings-all";
-    const columns = [
-        { headerName: "Guest Name", field: "nameguestid", sortable: true },
-        { headerName: "Host Name", field: "namehostid", sortable: true },
-        { headerName: "Service", field: "serviceId", sortable: true },
-        { headerName: "Start Date", field: "idsDate", sortable: true },
-        { headerName: "End Date", field: "iddDate", sortable: true },
-        { headerName: "Unit", field: "unitId", sortable: true },
-        { headerName: "Status", field: "status", sortable: true },
-        { headerName: "Duration", field: "duration", sortable: true },
-        { headerName: "total amount", field: "total_amount", sortable: true },
+    /*  const dataSourceName = "api/bookings-all";
+     const columns = [
+         { headerName: "Guest Name", field: "nameguestid", sortable: true },
+         { headerName: "Host Name", field: "namehostid", sortable: true },
+         { headerName: "Service", field: "serviceId", sortable: true },
+         { headerName: "Start Date", field: "idsDate", sortable: true },
+         { headerName: "End Date", field: "iddDate", sortable: true },
+         { headerName: "Unit", field: "unitId", sortable: true },
+         { headerName: "Status", field: "status", sortable: true },
+         { headerName: "Duration", field: "duration", sortable: true },
+         { headerName: "total amount", field: "total_amount", sortable: true },
+ 
+     ]; */
 
-    ];
-    
     return (
         <>
-            <Suspense fallback={<div>Loading...</div>}>
+            {/*  <Suspense fallback={<div>Loading...</div>}>
                 <GridCard dataSourceName={dataSourceName}
                     columns={columns}
                     isCreated={false}
@@ -33,7 +36,21 @@ function BookingsProvider() {
                         operator: ComparisonOperator.Equals,
                         value: "pending"
                     }]} />
-            </Suspense >
+            </Suspense > */}
+            <Tabber
+                tabsData={[
+                    {
+                        label: "Details Order",
+                        icon: <Deblur />,
+                        component: <Calendar /> ,
+                    },
+                    {
+                        label: "Details Service",
+                        icon: <Deblur />,
+                        component: <Calendar />,
+                    }
+                ]}
+            />
         </>
     )
 }
